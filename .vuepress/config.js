26ab5b46bae8e85c126895fc50c28c83814ad55b
Andrew Hawker <andrew.r.hawker@gmail.com>
2019-10-23 20:47:33 -0700

    Add themeconfig for navbar and sidebar

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/index.md' },
+            { text: 'How-to', link: '/how-to/' },
+            { text: 'Reference', link: '/reference/' },
+            { text: 'Topic', link: '/topic/' },
+            { text: 'Tutorial', link: '/tutorial/' },
+            { text: 'GitHub', link: 'https://github.com/getroutable/routable-docs' },
+        ],
+        sidebar: 'auto'
+    }
+  }
