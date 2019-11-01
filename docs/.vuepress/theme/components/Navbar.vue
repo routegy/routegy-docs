88262e0ed1141df678ad44de7c4da4df67e674d5
Andrew Hawker <andrew.r.hawker@gmail.com>
2019-11-01 11:38:22 -0700

    Add stylus color vars for the navbar component

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -102,12 +102,13 @@ $navbar-horizontal-padding = 1.5rem
   .site-name
     font-size 1.3rem
     font-weight 600
-    color $textColor
+    color $navbarTextColor
     position relative
   .links
     padding-left 1.5rem
     box-sizing border-box
-    background-color white
+    color $navbarTextColor
+    background-color $navbarBackgroundColor
     white-space nowrap
     font-size 0.9rem
     position absolute
