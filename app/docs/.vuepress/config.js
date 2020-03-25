dfdb54c9b2ad1296ff6bb6e0429ea71f84c13f5f
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-03-25 09:37:49 -0700

    Change docsDir path to app/docs so GitHub edit links are correct

--- app/docs/.vuepress/config.js
+++ app/docs/.vuepress/config.js
@@ -39,7 +39,7 @@ module.exports = {
         ],
         logo: '/images/web-logo-light.svg',
         repo: 'routegy/routegy-docs',
-        docsDir: 'docs',
+        docsDir: 'app/docs',
         editLinks: true,
         lastUpdated: true,
         footer: {
